aa175625680187a39409578d1986d067a0b8e0a5
Merlin-droid703 <melisa_aladag@hotmail.com>
2025-09-02 17:29:12 +0300

    [SP-5] Katmanlı Mimari

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+
+        return studentService.createStudent(student);
+    }
+}
