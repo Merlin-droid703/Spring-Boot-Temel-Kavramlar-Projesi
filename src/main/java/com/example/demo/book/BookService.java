9bbc2a95b179f4545f4fde2ab0226546e67dad6f
Merlin-droid703 <melisa_aladag@hotmail.com>
2025-09-02 17:44:42 +0300

    [SP-6] JPA Entity Basics

---
+++ src/main/java/com/example/demo/book/BookService.java
@@ -0,0 +1,22 @@
+package com.example.demo.book;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
